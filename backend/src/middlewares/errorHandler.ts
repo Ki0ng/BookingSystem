119f996747dd238fb89908334c7e150f4d7e64d4
Toan Le <toan.le@sapotacorp.vn>
2026-05-12 14:03:11 +0700

    add error

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import logger from '../utils/logger';
+import logger from '@/utils/logger';
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   let statusCode = err.status || 500;
@@ -14,16 +14,7 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
     message = 'Record not found.';
   }
 
-  // 🚀 Intelligent Logging
-  const logMessage = `[${req.method}] ${req.path} >> StatusCode: ${statusCode} >> ${message}`;
-  
-  if (statusCode >= 500) {
-    logger.error(logMessage, err);
-  } else if (statusCode >= 400) {
-    logger.warn(logMessage);
-  } else {
-    logger.info(logMessage);
-  }
+  logger.error(`[${req.method}] ${req.path} >> StatusCode: ${statusCode}`, err);
 
 
   res.status(statusCode).json({
